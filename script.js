aae522ca688770093cd3697aaa92103bba8c9a73
waqas <maqbool.webdev@gmail.com>
2026-03-25 08:35:11 +0500

    Added event listener to all buttons that show value on display

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-confirm('msg')
+const display = document.querySelector('input');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+    display.disabled = false;
+    display.value += button.textContent;
+    console.log(button);
+    });
+});
