9f1f039e281da91ec043e7fcac5dadcc568410f2
waqas <maqbool.webdev@gmail.com>
2026-04-02 10:46:33 +0500

    Added keyboard support

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ let secondNum = "";
 let currentOperator = null;
 let shouldResetScreen = false;
 
-function operate(op, a, b) {
+const operate = (op, a, b) => {
     a = parseFloat(a);
     b = parseFloat(b);
     switch (op) {
@@ -39,10 +39,7 @@ function operate(op, a, b) {
     }
 }
 
-digits.forEach(button => {
-    button.addEventListener('click', () => {
-        let value = button.textContent;
-
+const appendNumber = (value) => {
         if (shouldResetScreen) {
             display.textContent = '';
             shouldResetScreen = false;
@@ -57,29 +54,37 @@ digits.forEach(button => {
         } 
 
         display.textContent += value;
-    });
-});
+    };
 
+    digits.forEach(button => {
+        button.addEventListener('click', () => {
+            appendNumber(button.textContent);
+        })
+    });
+
+const handleOperator = (operator) => {
+    if (currentOperator !== null && display.textContent !== "") {
+        secondNum = display.textContent;
+        const result = operate(currentOperator, Number(firstNum), Number(secondNum));
+
+        display.textContent = Math.round(result * 1000) / 1000; // Round Off
+        firstNum = display.textContent;
+        secondNum = "";
+    } else {
+        firstNum = display.textContent;
+    }
+
+    currentOperator = operator;
+    shouldResetScreen = true;
+}
 
 allOperators.forEach(opButton => {
     opButton.addEventListener('click', () => {
-        if (currentOperator !== null && display.textContent !== "") {
-
-            secondNum = display.textContent;
-            const result = operate(currentOperator, firstNum, secondNum);
-            display.textContent = Math.round(result * 1000) / 1000; // Rounding off
-            firstNum = display.textContent;
-            secondNum = "";
-        } else {
-
-            firstNum = display.textContent;
-        }
-        currentOperator = opButton.textContent;
-        shouldResetScreen = true;
+        handleOperator(opButton.textContent);
     });
 });
 
-equal.addEventListener('click', () => {
+const calculate = () => {
     if (currentOperator === null || shouldResetScreen) return;
 
     secondNum = display.textContent;
@@ -89,17 +94,44 @@ equal.addEventListener('click', () => {
     firstNum = display.textContent;
     currentOperator = null;
     shouldResetScreen = true;
-});
+}
 
-clear.addEventListener('click', () => {
+const clearDisplay = () => {
     display.textContent = "0";
     firstNum = "";
     secondNum = "";
     currentOperator = null;
-});
+}
 
-backSpace.addEventListener('click', () => {
+const deleteNumber = () => {
     display.textContent = display.textContent.toString().slice(0, -1);
     if (display.textContent === "") display.textContent = "0";
-});
+}
 
+clear.addEventListener('click', clearDisplay);
+backSpace.addEventListener('click', deleteNumber);
+equal.addEventListener('click', calculate);
+
+// Keyboard Support
+
+const handleKeyboardInput = (e) => {
+    if(e.key === 'Escape') {
+        clearDisplay();
+    } else if(e.key === 'Backspace') {
+        deleteNumber();
+    } else if(e.key === '=' || e.key === 'Enter') {
+        e.preventDefault()
+        calculate();
+    } else if(['+','-','*','/'].includes(e.key)) {
+    let operator = e.key;
+
+    if (operator === '*') operator = '×';
+    if (operator === '/') operator = '÷';
+
+    handleOperator(operator);
+    } else if(e.key >= 0 && e.key <= 9 || e.key === '.') {
+        appendNumber(e.key);
+    }
+}
+
+window.addEventListener('keydown', handleKeyboardInput);
